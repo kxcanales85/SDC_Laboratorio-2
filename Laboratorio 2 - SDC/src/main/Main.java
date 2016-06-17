86a98b12fc6a30a44b1d547f54dbe0f5434c813d
kxcanales85 <kevin.canales@usach.cl>
2016-06-17 20:35:25 -0300

    Interfaz

--- Laboratorio 2 - SDC/src/main/Main.java
+++ Laboratorio 2 - SDC/src/main/Main.java
@@ -3,19 +3,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package laboratorio.pkg2.sdc;
+package main;
+import vistas.Vista_Principal;
 
 /**
  *
  * @author Kevin Canales
  */
-public class Laboratorio2SDC {
+public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Vista_Principal nueva = new Vista_Principal();
+        nueva.show();
     }
     
 }
